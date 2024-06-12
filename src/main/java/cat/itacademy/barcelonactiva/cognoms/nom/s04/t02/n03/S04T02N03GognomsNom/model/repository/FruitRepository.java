a9f80978cfededa4461ea4b1bff1b886f6e1eb45
Davide <infodadaisma@gmail.com>
2024-06-12 12:38:40 +0200

    progress

--- src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n03/S04T02N03GognomsNom/model/repository/FruitRepository.java
+++ src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n03/S04T02N03GognomsNom/model/repository/FruitRepository.java
@@ -1,10 +1,11 @@
 package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n03.S04T02N03GognomsNom.model.repository;
 
 import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n03.S04T02N03GognomsNom.model.domain.Fruit;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FruitRepository extends JpaRepository<Fruit, Integer> {
+public interface FruitRepository extends PagingAndSortingRepository<Fruit, Integer> {
 
 }
